5e0d97eb1a2522eeefd154c010728c78e39c68fc
Noo0teD <78922983+nooted@users.noreply.github.com>
2021-09-30 10:19:03 +0500

    Update example.py

--- example.py
+++ example.py
@@ -1,17 +1,16 @@
 import NextCloud
 import json
 
-url = "http://111.68.101.225/nextcloud/"
-userid = "nextcloud"
-passwd = "nextcloud"
-#passwd = "m8wb5-4ce5D-5wtQc-8QtHP-dmXz4"
+url = ""
+userid = ""
+passwd = ""
 
 #True if you want to get response as JSON
 #False if you want to get response as XML
 tojs = True
 
 nxc = NextCloud.NextCloud(url,userid,passwd,tojs)
-#dictt = (nxc.createShare(path="t1.md" , shareType=3 , shareWith=None, publicUpload=False , password=None ,  permissions=None))
+#dictt = (nxc.createShare(path="" , shareType=3 , shareWith=None, publicUpload=False , password=None ,  permissions=None))
 print = (nxc.getShares())
 #print (dictt['ocs']['data']['url'])
 #print (dictt)
